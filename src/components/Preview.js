2b00538b4ad69edce2648ac81d18895c813ab2f5
Ritabrata Patra <teamritabrata@gmail.com>
2024-11-23 18:59:39 +0530

    created business card component and completed it

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import BusinessCard from './BusinessCard'
 
 const Preview = ({Values}) => {
   return (
     <section className="Preview">
       <h2>Preview</h2>
       <pre>{JSON.stringify(Values, null, 2)}</pre>
+      <div className="Preview-wrapper">
+      <BusinessCard Values={Values}/> 
+      </div>
+      
     </section>
   )
 }
